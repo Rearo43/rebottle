b9e410651da5c6ac6b1326741f93b0bd210d03e1
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2022-01-06 20:37:05 -0800

    files connected

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,12 @@
-// 'use strict';
+'use strict';
 
 $('.wwwButton').click(function () {
   let input = parseFloat($('#www').val());
-  console.log(input);
-  let lb = (input / 16);
-  console.log(lb);
+  // console.log(input);
+  let lb = input / 16;
+  // console.log(lb);
   let final = lb + input;
-  console.log(input);
-  console.log(final);
+  $(".answer").text(final);
+  // console.log(input);
+  // console.log(final);
 });
--- server.js
+++ server.js
@@ -3,3 +3,40 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3005;
+
+const express = require('express');
+const pg = require('pg');
+const morgan = require('morgan');
+
+const app = express();
+const database = new pg.Client(process.env.DATABASE_URL);
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', home);
+app.use('*', routeNotFound);
+app.use(bigError);
+
+function home(req, res) {
+  res.status(200).render('pages/home');
+}
+
+//----------404 Error
+function routeNotFound(req, res) {
+  res.status(404).send('Route NOT Be Found!');
+}
+
+//----------All Errors minus 404
+function bigError(error, res) {
+  console.log(error);
+  res.status(error).send('BROKEN!');
+}
+
+//----------Connect to Server and Database
+database.connect(() => {
+  app.listen(PORT, () => console.log(`WORKING!: ${PORT}`));
+});
